0c2c01cd87030c582700362479710213d8eaafb4
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2023-06-13 19:03:37 +0200

    from hw-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,78 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import { nanoid } from 'nanoid';
+
+import Section from './Section';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import FilterForm from './FilterForm';
+
+export default class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  createPerson = person => {
+    const newContact = {
+      id: nanoid(),
+      name: person.name,
+      number: person.number,
+    };
+
+    if (
+      this.state.contacts.some(
+        person => person.name.toLowerCase() === newContact.name.toLowerCase()
+      )
+    ) {
+      alert(`${newContact.name} is already in contacts.`);
+    } else {
+      this.setState({
+        contacts: [...this.state.contacts, { ...newContact }],
+      });
+    }
+  };
+
+  deletePerson = delPersonId => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(({ id }) => id !== delPersonId),
+    }));
+  };
+
+  setFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  getFilteredPersons = () => {
+    const filter = this.state.filter.toLowerCase();
+    const allPersons = this.state.contacts;
+    const filteredPersons = allPersons.filter(person =>
+      person.name.toLowerCase().includes(filter)
+    );
+    return filteredPersons;
+  };
+
+  render() {
+    const filteredPersons = this.getFilteredPersons();
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <Section>
+          <ContactForm createPerson={this.createPerson}></ContactForm>
+        </Section>
+        <Section title="Contacts">
+          <FilterForm setFilter={this.setFilter}></FilterForm>
+          <ContactList
+            contacts={filteredPersons}
+            deletePerson={this.deletePerson}
+          ></ContactList>
+        </Section>
+      </div>
+    );
+  }
+}
